fd25fbf5e1cc87edd7700ae6e4f79a5688325b94
Lanselord <nosirovhumoyiddin23@gmail.com>
2023-11-12 14:03:50 +0300

    Java Script

---
+++ main/js/main.js
@@ -0,0 +1,12 @@
+var menu = String (prompt("Menu:" , "Shashlik,Manti,Qozonkabob"))
+console.log(menu)
+
+var suv = String (prompt("Ichimlik: Cola,Pepsi,Nestle,Fanta,choy"))
+console.log(suv)
+
+var javob = [
+    "Siz yegan ovqat: " + " " + menu + "\n" +
+    "Siz ichgan ichimlik: " + " " + suv + "\n"
+]
+
+alert (javob)
